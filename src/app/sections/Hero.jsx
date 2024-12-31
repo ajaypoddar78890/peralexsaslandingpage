d4ec52219b81478d01a39308c2b9e7ae64e885f8
ajaypoddar78890 <ajay.poddar15fea@gmail.com>
2024-12-31 15:41:12 +0530

    adding the hero imag

---
+++ src/app/sections/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="Hero-section container-fluid h-full w-full">
+      <div className="container mx-auto px-5">
+        <div className="hero-content "></div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;

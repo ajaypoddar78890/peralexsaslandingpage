9254b76273dd62fb1970fc572ad4cdad6199af23
ajaypoddar78890 <ajay.poddar15fea@gmail.com>
2025-01-02 10:40:57 +0530

    fixed the git branhced

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -10,41 +10,54 @@ const Hero = () => {
             <div className="button text-white my-3 ">
               <Link
                 href="#"
-                className="bg-white  text-gray-600 font-regular px-5 py-3 rounded-lg shadow-md   hover:shadow-lg transition-all duration-300"
+                className="bg-white  text-gray-600 font-regular px-5 py-3 rounded-[50px] shadow-md   hover:shadow-lg transition-all duration-300"
               >
                 Designing for the Future
               </Link>
             </div>
-            <div className="contant my-10 ">
-              <h1 className="text-3xl">
-                revolutionize Your <span>Workflow With Sass</span>
+            <div className="contant my-8 ">
+              <h1 className="text-3xl font-semibold ">
+                Revolutionize Your{" "}
+                <span className="text-blue-700">Workflow </span>With Sass
               </h1>
-              <p className="mt-5 text-base">
-                And in Order to make a business , brand advertising and
-                marketing plays an important role similarly , in making
-                cultivfation business are neccesaary{" "}
+              <p className="mt-2 text-base text-gray-600 ">
+                And in Order To Make A Business , Brand Advertising And
+                Marketing Plays an Important Role Similarly , in Making
+                Cultivfation Business Are Neccesaary{" "}
               </p>
             </div>
-            <Link
-              href="#"
-              className="py-3 px-6 border-2 border-blue-600 text-black font-medium rounded-lg hover:text-white hover:bg-blue-600 transition duration-300 mt-3"
-            >
-              DiscoverMore +
-            </Link>
-            <Link
-              href="#"
-              className="my-5 text-black font-medium flex items-center hover:text-blue-700 transition duration-300"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="black"
-                viewBox="0 0 24 24"
-                className="w-7 h-7 mr-2"
-              >
-                <path d="M5 3.5v17l14.5-8.5L5 3.5z" />
-              </svg>
-              How it works
-            </Link>
+            <div className="container mx-auto mt-5">
+              <div className="flex flex-col sm:flex-row justify-center gap-4">
+                <Link
+                  href="#"
+                  className="py-3 px-6 border-2 border-blue-600 text-black font-medium rounded-lg hover:text-white hover:bg-blue-600 transition duration-300 mt-3 inline-block"
+                >
+                  DiscoverMore +
+                </Link>
+
+                <Link
+                  href="#"
+                  className="py-3 px-6 text-black font-medium flex items-center hover:text-blue-700 transition duration-300 border-blue-800 border-2  "
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="w-10 h-10 bg-white rounded-full p-2"
+                  >
+                    <circle
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="black"
+                      stroke-width="3"
+                    />
+                    <polygon points="10,8 16,12 10,16" fill="black" />
+                  </svg>
+                  How it works
+                </Link>
+              </div>
+            </div>
           </div>
           <div className="bg-green-500 p-4">Grid 2</div>
         </div>

14febed66921df0e9662253ad38e6f16cd2291ea
ajaypoddar78890 <ajay.poddar15fea@gmail.com>
2025-01-06 15:49:19 +0530

    both the section are fixed now

--- src/app/sections/Header.tsx
+++ src/app/sections/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className=" bg-transparent absolute top-0 left-0   w-full z-50 ">
+    <header className=" bg-transparent absolute top-2 left-0   w-full z-50 ">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
         <div className="text-4xl font-bold text-blue-600">MoreDev</div>
 

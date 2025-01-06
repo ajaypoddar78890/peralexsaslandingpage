4950aa2322d0ef8d86cb24976cede3633dfcd973
ajaypoddar78890 <ajay.poddar15fea@gmail.com>
2025-01-06 16:18:25 +0530

    added the serice section

--- src/app/sections/services.jsx
+++ src/app/sections/services.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const services = () => {
-  return <div>hey service page</div>;
+  return (
+    <div className="services-section bg-gradient-to-b from-blue-50 to-blue-100 h-full w-full ">
+      <div className="container px-5 mx-auto lg:py-[2rem]">
+        <div className="content max-w-3xl mx-auto text-center">
+            <h4 className="font-semibold text-xl text-blue-700 md:text-2xl lg:text-3xl ">Advance Services</h4>
+            <h1 className="font-bold text-xl md:text-2xl lg:text-5xl my-2 lg:my-5">Optimize  Your <span className="text-blue-700">Performance</span>  with Sass</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default services;
